46bbf1e88be3c0fddd20d9cae8ad2a9d2f5aae8e
Navapu <drojannp2@gmail.com>
2023-05-27 03:34:37 +0200

    Añado mi icono, tipo de letra y empiezo la pagina de inicio

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ const router = express.Router()
 router.get('/', (req, res) =>{
     res.render('index.hbs')
 })
+router.get('/carta', (req, res) =>{
+    res.render('carta.hbs')
+})
 module.exports = router
