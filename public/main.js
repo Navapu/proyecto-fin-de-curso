23679ad6e1f110b8633d62b327ebc69a34f7bd5d
Navapu <drojannp2@gmail.com>
2023-06-02 02:39:18 +0200

    Añado productos

--- public/main.js
+++ public/main.js
@@ -14,3 +14,28 @@ function btn2(){
     boton1.style.backgroundColor = "white"
     boton1.style.border = "1px dashed black"
 }
+function abrir(){
+    let especialidad = document.getElementById('especialidad')
+    let entrante = document.getElementById('entrante')
+    let sopa = document.getElementById('sopa')
+    especialidad.style.display = 'block'
+    entrante.style.display = 'none'
+    sopa.style.display = 'none'
+
+}
+function aentrante(){
+    let especialidad = document.getElementById('especialidad')
+    let entrante = document.getElementById('entrante')
+    let sopa = document.getElementById('sopa')
+    especialidad.style.display = 'none'
+    entrante.style.display = 'block'
+    sopa.style.display = 'none'
+}
+function asopa(){
+    let especialidad = document.getElementById('especialidad')
+    let entrante = document.getElementById('entrante')
+    let sopa = document.getElementById('sopa')
+    especialidad.style.display = 'none'
+    entrante.style.display = 'none'
+    sopa.style.display = 'block'
+}
