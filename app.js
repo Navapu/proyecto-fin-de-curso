9698508e2c87d38db54b00f5690cd724947d95c8
Navapu <drojannp2@gmail.com>
2023-06-04 20:21:09 +0200

    Test deploy v5

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const myconecction = require('express-myconnection')
 const bodyParser = require('body-parser')
 const pool = require ('./database.js')
 const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');const app = express()
+const mysqlStore = require('express-mysql-session')(session);
+const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', engine({
   defaultLayout: 'main',
@@ -16,14 +16,12 @@ app.engine('.hbs', engine({
   helpers: require('./lib/handlebars')
 }))
 
-const client = redis.createClient(); // Crea una instancia del cliente de Redis
-
 app.use(session({
-  store: new RedisStore({ client: client }), // Configura el almacén de sesiones de Redis
-  secret: 'tfg', // Reemplaza con tu propia clave secreta
+  secret: 'tfgsession',
   resave: false,
-  saveUninitialized: false
-}));
+  saveUninitialized: false,
+  store: new mysqlStore({},pool)
+}))
 
 app.set('view engine', '.hbs');
 app.use(express.urlencoded({ extended: false }))
